ff2a29d11e1c1973568459086a9379f59ab6167b
Ulyana Shabunina <66304635+UlyanaShabunina@users.noreply.github.com>
2021-04-29 19:58:42 +0300

    Add files via upload

---
+++ Task3/Task3/Task3.cpp
@@ -0,0 +1,27 @@
+﻿#include<stdio.h>
+#include<stdlib.h>
+#include<math.h>
+#include <iostream>
+
+double f(double x) {
+	return 3*x - cos(x) - 1;
+}
+
+double df(double x) {
+	return sin(x) + 3;
+}
+
+double getRoot(double x) {
+	double x_next = x - f(x) / df(x);
+	if (fabs(x_next - x) < pow(10, -5)) {
+		return x_next;
+	}
+	return getRoot(x_next);
+}
+using namespace std;
+int main() {
+	setlocale(LC_ALL, "Russian");
+	cout << "1 корень : " << getRoot(1) << endl;
+	system("pause");
+	return 0;
+}
